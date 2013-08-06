3680924f9ab8f98074f86fea4aa93327902290f5
Ian Johnson <enjalot@gmail.com>
2013-08-05 23:00:23 -0700

    rearrange code slightly. add simple 'test' script

--- server.js
+++ server.js
@@ -12,64 +12,71 @@ app.configure(function() {
   app.use(app.router);
   return
 });
-
-var client = knox.createClient({
+if(process.env['AWS_ACCESS_KEY_ID']) {
+  var client = knox.createClient({
     key    : process.env['AWS_ACCESS_KEY_ID']
   , secret : process.env['AWS_SECRET_ACCESS_KEY']
   , bucket : process.env['AWS_BUCKET_NAME']
 });
+} else {
+  var conf = require('./conf')
+  var client = knox.createClient(conf.aws)
+}
+
 
 app.post('/', function(req, res, next){
 
 	var filename_id = req.body.id;
 	var svg_name = filename_id + '.svg'
 	var png_name = filename_id + '.png'
-	
+
 	var svg_path = __dirname + '/tmp/' + svg_name
 	var png_path = __dirname + '/tmp/' + png_name
 
 	var svg_doctype = '<?xml version=\"1.0" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">';
 	var svg_decoded = new Buffer(req.body.source, 'base64').toString();
 	var svg_source = svg_doctype + svg_decoded
+  console.log("decoded", svg_decoded)
 
 	var temp_svg = fs.writeFile(svg_path, svg_source, function(err){
 		if (err) console.log(err);
 		var writable = fs.createWriteStream(png_path);
-		writable.on('close', function(){
-			var temp_png = fs.readFile(png_path, function(err, file){
-				if (err) console.log(err);
-				client.putFile(png_path, png_name, { 'Content-Type' : 'image/png' }, function (err, result) {
-					if (err) console.log(err);
-					if (result){
-						console.log('PNG saved to Amazon S3');
-						result.resume();
-						result.on('end', function(){
-							console.log('Connection closed');
-							fs.unlink(svg_path, function (err) {
-								if (err) console.log(err);
-								fs.unlink(png_path, function (err) {
-									if (err) console.log(err);
-									return;
-								});
-							});
-						});
-					}
-				});
-			});
-		});
+		writable.on('close', written);
+
 		readable = fs.createReadStream(svg_path);
 		readable
       .pipe(new Inkscape(['-e']))
-      .pipe(writable);		
+      .pipe(writable);
+
+    function written() {
+      client.putFile(png_path, png_name, { 'Content-Type' : 'image/png' }, function (err, result) {
+        if (err) console.log(err);
+        if (result){
+          console.log('PNG saved to Amazon S3');
+          result.resume();
+          result.on('end', function(){
+            console.log('Connection closed');
+            fs.unlink(svg_path, function (err) {
+              if (err) console.log(err);
+              fs.unlink(png_path, function (err) {
+                if (err) console.log(err);
+                return;
+              });
+            });
+          });
+        }
+      });
+    }
 	});
-	
+
 	res.format({
 		json: function(){
 			res.send('SVG/PNG files saved\n');
 		}
 	});
-	
+
 })
+
 
 var port = process.env['PORT'] || 4444
 exports.startServer = function() {
