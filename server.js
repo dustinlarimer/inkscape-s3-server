dd9068eb207516c45a9e31a22318b55fd0ed6cfd
Dustin Larimer <dustinlarimer@gmail.com>
2013-08-20 10:37:50 -0700

    Commented out console.log of decoded svg

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/', function(req, res, next){
 	var svg_doctype = '<?xml version=\"1.0" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">';
 	var svg_decoded = new Buffer(req.body.source, 'base64').toString();
 	var svg_source = svg_doctype + svg_decoded
-  console.log("decoded", svg_decoded)
+  //console.log("decoded", svg_decoded)
 
 	var temp_svg = fs.writeFile(svg_path, svg_source, function(err){
 		if (err) console.log(err);
